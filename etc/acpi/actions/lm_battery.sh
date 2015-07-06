7fb1704523dc4f9af6a466afabf167149d22bac2
Ritesh Raj Sarraf <rrs@debian.org>
2015-07-06 17:47:06 +0530

    Same here, use lmt-udev for invocation

--- etc/acpi/actions/lm_battery.sh
+++ etc/acpi/actions/lm_battery.sh
@@ -1,8 +1,7 @@
 #! /bin/sh
 
-test -f /usr/sbin/laptop_mode || exit 0
+test -f /lib/udev/lmt-udev || exit 0
 
 # Automatically disable laptop mode when the battery almost runs out,
 # and re-enable it when it
-
-/usr/sbin/laptop_mode auto
+/lib/udev/lmt-udev
